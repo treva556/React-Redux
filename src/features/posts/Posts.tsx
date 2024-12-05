5a846c19d7ef069438246128c466597a7a47a6fd
treva556 <marktreva92@gmail.com>
2024-12-06 02:57:26 +0300

    posts fetch

--- src/features/posts/Posts.tsx
+++ src/features/posts/Posts.tsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {useAppSelector } from "../../app/hooks";
+import Post from  './Post'
+import PostForm from './PostForm'
+import { fetchPostsAsync, selectPosts, selectStatus, Statuses, updatePostAsync } from './postSlice';
 
-const Posts = () => {
-  return (
-    <div>
-        
-        Posts
+function Posts() {
+  const posts = useAppSelector(selectPosts);
+  const status = useAppSelector(selectStatus)
+  const dispatch = useDispatch();
 
-    </div>
-  )
+  const [postToEdit, setPostToEdit] = useState(0);
+
+  useEffect(() => {
+    dispatch(fetchPostsAsync());
+  }, [dispatch])
+
+  function toggleEditForm(post_id?:number) {
+      if (postToEdit === post_id) {
+          setPostToEdit(0);
+      } else {
+            setPostToEdit(post_id as number);
+      }
+  }
+
+  function submitEdit(formData:any) {
+      dispatch(updatePostAsync(formData));
+      toggleEditForm();
+  }
+
+  let contents;
+
+  if (status !== Statuses.UpToDate) {
+    contents = <div>{status}</div>
+  } else {
+      contents = <div className="card">
+        <div className="card-body">
+            <h3>{status}</h3>
+            <PostForm />
+            {posts && posts.length > 0 && posts.map(post => {
+                return <div key={post.id} style={{margin:"5em"}}>
+                    <Post 
+                        dispatch={dispatch}
+                        post={post}
+                        toggleEditForm={() => toggleEditForm(post.id)}
+                        postToEdit={postToEdit}
+                        submitEdit={submitEdit}
+                    />
+                </div>
+            })}
+            </div>
+      </div>
+  }
+
+  return <div><h1>Posts</h1>
+        {contents}
+  </div>
 }
 
-export default Posts
+export default Posts;
