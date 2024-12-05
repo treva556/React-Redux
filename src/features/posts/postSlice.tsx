1db3c3d6f823bd45d04bb4a8e44d1e6b71a07221
treva556 <marktreva92@gmail.com>
2024-12-05 21:17:49 +0300

    first commit

---
+++ src/features/posts/postSlice.tsx
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import produce from "immer";
+import { RootState } from "../../app/store";
+import { initializeConnect } from "react-redux/es/components/connect";
+
+
+export enum Statuses {
+      Initial = " Not Fetched", 
+      Loading = " Loading...",
+      UpToDate = "Up To Date",
+      Deleted = " Deleted",
+      Error = "Error",
+}
+
+export interface PostState {
+  id?: number;
+  title?: string;
+  body?: string;
+  created_at?: any;
+  updated_at?: any;
+  
+}
+
+export interface PostsState {
+    posts: PostState[];
+    status: string;
+
+}
+
+
+const initialState: any = {
+     posts: [
+        {
+            id: 0,
+            title: "",
+            body: "",
+            created_at: "",
+            updated_at: "",
+        }
+     ],
+     status: Statuses.Initial
+}; 
+
+//// 
